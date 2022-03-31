2e5094b99e972949961d94cd9ddf7fcf629c1a22
slon11235 <ultramarinorange@gmail.com>
2022-03-31 16:01:46 +0000

    Now it can measure size

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,7 @@ else
 	echo  "$dir directory does not exist, exiting"
 	exit 101
 fi
+echo -e "The heaviest subfolder of $dir is:"
+top1=$(du -S $dir 2>/dev/null | sort -rn | head -n 1 | awk '{print $2 "\n"}')
+disk=$(du -Sh -d 0 $top1 2>/dev/null | awk '{print $1}')
+echo -e "$top1 with disk usage of $disk"
